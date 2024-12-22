72be2794a1b8cfa85eb9d6924f0d40cdcd1cedfb
Kerem Yigit Barın <93515568+KeremYigitBarin@users.noreply.github.com>
2024-12-22 14:18:03 +0300

    kafka_producer_consumer.py

--- kafka_producer_consumer.py
+++ kafka_producer_consumer.py
@@ -6,16 +6,16 @@ import pandas as pd
 producer = KafkaProducer(bootstrap_servers='localhost:9092',
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
-# Veri setini okuyun
+# Veri seti
 file_path = r"C:\Users\kyb20\OneDrive\Masaüstü\Netflix_Data.csv"
 data = pd.read_csv(file_path)
 
-# Anomali tespiti ekleyin
+# Anomali tespiti
 data['Price_Change'] = data['Adj_Close'].diff().fillna(0)
 threshold = data['Price_Change'].std()
 data['Anomaly'] = (data['Price_Change'].abs() > threshold).astype(int)
 
-# Veriyi Kafka'ya gönderin
+# Verinin Kafka'ya gönderimi
 for index, row in data.iterrows():
     producer.send('anomaly_data', value=row.to_dict())
     print(f"Gönderilen veri: {row.to_dict()}")
